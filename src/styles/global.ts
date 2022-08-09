74d33aee313e7b785acc9f9fbaadc89ad9172ac0
Wallysson <wlc.couto@gmail.com>
2022-08-09 20:41:23 -0300

    installing styled-components, crate global.ts, create theme.ts and its types, create global.ts

---
+++ src/styles/global.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: 0;
+  }
+
+  body {
+    background: ${props => props.theme.background};
+    color: ${props => props.theme['base-text']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button, textarea {
+    font-family: 'Roboto', sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+  }
+`
